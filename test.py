535b6ac840693e709bd172294177b7542c4a5c38
artzuros <gunubansal129@gmail.com>
2024-08-02 16:02:44 +0530

    using clip-vit-large-patch14-336, updated requirements

--- test.py
+++ test.py
@@ -10,19 +10,24 @@ from torchvision import transforms
 import streamlit as st
 import pandas as pd
 
-from utils.utils import ImageDataset, normalizeL2, add_vector_to_index, extract_features_dino, extract_features_clip, search_index_image, search_index_text
+from utils.utils import (
+    normalizeL2,
+    add_vector_to_index,
+    extract_features_dino,
+    extract_features_clip,
+    search_index_image,
+    search_index_text,
+)
 
-transform = transforms.Compose([
-    transforms.ToTensor()
-])
+transform = transforms.Compose([transforms.ToTensor()])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load models and processors
-processor_dino = AutoImageProcessor.from_pretrained('facebook/dinov2-base')
-model_dino = AutoModel.from_pretrained('facebook/dinov2-base').to(device)
+processor_dino = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+model_dino = AutoModel.from_pretrained("facebook/dinov2-base").to(device)
 
-processor_clip = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
-model_clip = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+processor_clip = AutoProcessor.from_pretrained("openai/clip-vit-large-patch14-336")
+model_clip = CLIPModel.from_pretrained("openai/clip-vit-large-patch14-336").to(device)
 
 # Collect all images
 images = []
@@ -32,27 +37,30 @@ DIR_PATH = st.text_input("Enter the directory path:")
 if DIR_PATH:
     for root, dirs, files in os.walk(DIR_PATH):
         for file in files:
-            if file.endswith('.jpg') or file.endswith('.png'):
+            if file.endswith(".jpg") or file.endswith(".png"):
                 images.append(os.path.join(root, file))
+    length = (len(images))
 
 # Function to generate FAISS index
 def generate_faiss_index(images, index_clip, index_dino):
     for image in images:
-        img = Image.open(image).convert('RGB')
+        img = Image.open(image).convert("RGB")
         clip_features = extract_features_clip(img)
         add_vector_to_index(clip_features, index_clip)
 
         dino_features = extract_features_dino(img)
         add_vector_to_index(dino_features, index_dino)
 
-    faiss.write_index(index_clip, DIR_PATH + '/' + 'index_clip.index')
-    faiss.write_index(index_dino, DIR_PATH + '/' + 'index_dino.index')
+    faiss.write_index(index_clip, DIR_PATH + "/" + "index_clip.index")
+    faiss.write_index(index_dino, DIR_PATH + "/" + "index_dino.index")
 
 # Load FAISS index
 def load_faiss_index():
-    if os.path.exists(DIR_PATH + '/' + 'index_clip.index') and os.path.exists(DIR_PATH + '/' + 'index_dino.index'):
-        index_clip = faiss.read_index(DIR_PATH + '/' + 'index_clip.index')
-        index_dino = faiss.read_index(DIR_PATH + '/' + 'index_dino.index')
+    if os.path.exists(DIR_PATH + "/" + "index_clip.index") and os.path.exists(
+        DIR_PATH + "/" + "index_dino.index"
+    ):
+        index_clip = faiss.read_index(DIR_PATH + "/" + "index_clip.index")
+        index_dino = faiss.read_index(DIR_PATH + "/" + "index_dino.index")
         return index_clip, index_dino
     else:
         st.error("FAISS index files not found!")
@@ -60,7 +68,7 @@ def load_faiss_index():
 
 # Streamlit buttons
 if st.button("Generate FAISS Index"):
-    index_clip = faiss.IndexFlatL2(512)
+    index_clip = faiss.IndexFlatL2(768)
     index_dino = faiss.IndexFlatL2(768)
     generate_faiss_index(images, index_clip, index_dino)
     st.session_state.index_clip = index_clip
@@ -75,7 +83,7 @@ if st.button("Load FAISS Index"):
         st.success("FAISS index loaded successfully!")
 
 # Ensure the indices are defined before performing search
-if 'index_clip' in st.session_state and 'index_dino' in st.session_state:
+if "index_clip" in st.session_state and "index_dino" in st.session_state:
     index_clip = st.session_state.index_clip
     index_dino = st.session_state.index_dino
 
@@ -84,10 +92,11 @@ if 'index_clip' in st.session_state and 'index_dino' in st.session_state:
 
     if query:
         # Perform CLIP text search
-        D_clip_text, I_clip_text = search_index_text(query, index_clip)
-        
+        D_clip_text, I_clip_text = search_index_text(query, index_clip, length)
+
         # Slider to control the number of images to display
-        total_images_to_show = st.slider("Total images to show", 10, min(len(images), 200), 10, 10)
+        max_images_to_show = min(len(I_clip_text[0]), len(images))
+        total_images_to_show = st.slider("Total images to show", 20, max_images_to_show, 20, 20)
 
         st.session_state.selected_images = st.session_state.get("selected_images", [])
         st.session_state.selected_distances = st.session_state.get("selected_distances", [])
@@ -95,14 +104,15 @@ if 'index_clip' in st.session_state and 'index_dino' in st.session_state:
         cols = st.columns(5)  # Adjust the number of columns as needed
 
         for idx in range(total_images_to_show):
-            image_path = images[I_clip_text[0][idx]]
+            image_index = I_clip_text[0][idx]
+            image_path = images[image_index]
             img = Image.open(image_path)
             col = cols[idx % 5]
-            
+
             with col:
                 st.image(img, use_column_width=True)
                 st.write(f"Distance: {D_clip_text[0][idx]:.3f}")
-                
+
                 if st.checkbox(f"Select", key=f"select_{idx}"):
                     if image_path not in st.session_state.selected_images:
                         st.session_state.selected_images.append(image_path)
@@ -122,15 +132,15 @@ if 'index_clip' in st.session_state and 'index_dino' in st.session_state:
 
             df = pd.DataFrame(data, columns=["Query", "Image", "Distance"])
             timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            df.to_csv(DIR_PATH + '/' + timestamp + "selected_images_clip.csv", index=False)
+            df.to_csv(DIR_PATH + "/" + timestamp + "selected_images_clip.csv", index=False)
             st.success("Selected images exported to CSV!")
 
         if st.button("Process Selected Images with DINO"):
             st.session_state.dino_search_results = []
 
             for selected_image in st.session_state.selected_images:
-                img = Image.open(selected_image).convert('RGB')
-                D_dino, I_dino = search_index_image(img, index_dino)
+                img = Image.open(selected_image).convert("RGB")
+                D_dino, I_dino = search_index_image(img, index_dino, length)
                 for d, i in zip(D_dino[0], I_dino[0]):
                     st.session_state.dino_search_results.append((selected_image, images[i], d))
 
@@ -140,7 +150,9 @@ if 'index_clip' in st.session_state and 'index_dino' in st.session_state:
 
             st.write("Top matching images using DINO:")
             cols = st.columns(5)
-            for idx, (selected_image, similar_image, distance) in enumerate(st.session_state.dino_search_results):
+            for idx, (selected_image, similar_image, distance) in enumerate(
+                st.session_state.dino_search_results
+            ):
                 col = cols[idx % 5]
                 with col:
                     img = Image.open(similar_image)
@@ -155,9 +167,11 @@ if 'index_clip' in st.session_state and 'index_dino' in st.session_state:
                 for query_image, similar_image, distance in st.session_state.dino_search_results:
                     data.append([query, query_image, similar_image, distance])
 
-                df = pd.DataFrame(data, columns=["Query", "Selected Image", "Similar Image", "Distance"])
+                df = pd.DataFrame(
+                    data, columns=["Query", "Selected Image", "Similar Image", "Distance"]
+                )
                 timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                df.to_csv(DIR_PATH + '/' + timestamp + "selected_images_dino.csv", index=False)
+                df.to_csv(DIR_PATH + "/" + timestamp + "selected_images_dino.csv", index=False)
                 st.success("DINO results exported to CSV!")
 
 else:
--- utils/utils.py
+++ utils/utils.py
@@ -13,23 +13,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 processor_dino = AutoImageProcessor.from_pretrained('facebook/dinov2-base')
 model_dino = AutoModel.from_pretrained('facebook/dinov2-base').to(device)
 
-processor_clip = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
-model_clip = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+processor_clip = AutoProcessor.from_pretrained("openai/clip-vit-large-patch14-336")
+model_clip = CLIPModel.from_pretrained("openai/clip-vit-large-patch14-336").to(device)
 
-class ImageDataset(Dataset):
-    def __init__(self, image_paths, transform=None):
-        self.image_paths = image_paths
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
-        image = Image.open(img_path).convert('RGB')
-        if self.transform:
-            image = self.transform(image)
-        return image, img_path
 
 def normalizeL2(embeddings):
     vector = embeddings.detach().cpu().numpy()
@@ -62,21 +48,21 @@ def extract_features_clip_text(text):
         text_features = model_clip.get_text_features(**inputs)
         return text_features
     
-def search_index_text(text_query, index_clip):
+def search_index_text(text_query, index_clip, length):
     with torch.no_grad():
         inputs = processor_clip(text=text_query, return_tensors="pt").to(device)
         text_features = model_clip.get_text_features(**inputs)
     
     input_features_clip = normalizeL2(text_features)
     
-    D_clip_text, I_clip_text = index_clip.search(input_features_clip, 10)
+    D_clip_text, I_clip_text = index_clip.search(input_features_clip,length)
     
     return D_clip_text, I_clip_text
 
-def search_index_image(image, index_dino):
+def search_index_image(image, index_dino, length):
     dino_features = extract_features_dino(image)
     dino_features = normalizeL2(dino_features)
-    D_dino, I_dino = index_dino.search(dino_features, 10)
+    D_dino, I_dino = index_dino.search(dino_features, length)
     
     return D_dino, I_dino
     
